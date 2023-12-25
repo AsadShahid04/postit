7776afb6de0db1ed61aff146b577fb3014659be8
Asad Shahid <62490374+AsadShahid04@users.noreply.github.com>
2023-12-24 22:29:42 -0800

    Working Authentication with Google

--- prisma/client.js
+++ prisma/client.js
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = globalThis.prisma || new PrismaClient() // we are creating a new client if it doesn't exist
-if (process.env.NODE_ENV === "production") globalThis.prisma = client
+const client = globalThis.prisma || new PrismaClient(); // Create client if it doesn't exist
 
-export defualt client
+if (process.env.NODE_ENV === "production") {
+  globalThis.prisma = client;
+}
+
+export default client; // Export the client as the default value
