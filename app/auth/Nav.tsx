6e951a7788439dfe9e8edf4fcdb57ed35b1b4ed7
Asad Shahid <62490374+AsadShahid04@users.noreply.github.com>
2023-12-26 16:44:05 -0800

    Added frontend side of post feature

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link'
 import Login from "./Login"
+import {getServerSession} from "next-auth/next"
+import {authOptions} from "../../pages/api/auth/[...nextauth]"
+import Logged from './Logged'
 
 export default async function Nav() {
+    const session = await getServerSession(authOptions)
     return (
         <nav className="flex justify-between items-center py-8">
             <Link href={"/"}>
                 <h1 className ="font-bold text-lg"> Send it.</h1>                
             </Link>
             <ul className = "flex items-center gap-6">
+                {!session?.user && <Login />}
+                {session?.user && <h1> Hello, {session.user.name} </h1>}
+                {session?.user && <Logged image={session.user?.image || ""} />}
                 <Login />
             </ul>
         </nav>
